419215d9d01e5909ebc6673b318835354beacc7f
Yakoloi <yakoloi@gmail.com>
2017-06-28 13:05:57 -0700

    reworked

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,51 +1,108 @@
-var wordArray = ['cat', 'dog', 'sheep', 'crow', 'cow', 'pig', 'racoon'];
-var randomWord = wordArray[Math.floor(Math.random() * wordArray.length)];
-console.log(randomWord);
-var answerArray = [];
-var playerGuess;
-var score = 0;
-var letterCount = randomWord.length;
-var playerGuess;
-var guessCanvas = "";
-console.log("guesscanvas: " + guessCanvas)
+var gameStart = false;
+var pause = true;
+//game start function press enter to start//
+console.log("Press enter to start");
 
-for (var i = 0; i < randomWord.length; i++) {
-  answerArray[i] = "*";
-  document.getElementById("demo").innerHTML = answerArray.join(" ");
-}
-console.log("answerArray: " + answerArray)
-
-document.getElementById("score").innerHTML = score;
-
-
-
-var guessed;
-
-
-
-document.onkeyup = function(event) {
-
-  playerGuess = event.key;
-  guessed = guessCanvas.includes(playerGuess);
-  console.log("guessed boolean: " + guessed);
-  console.log("Guess Canvas " + guessCanvas);
-  guessCanvas = guessCanvas + event.key;
-//if guess is true do not execute this
-if (guessed === false){
-  for (var i = 0; i < randomWord.length; i++) {
-    if (randomWord[i] == playerGuess) {
-      answerArray[i] = playerGuess;
-      console.log("randomWord indexed letter: " + randomWord[i]);
-      console.log("answerArray status: " + answerArray[i]);
-      document.getElementById("demo").innerHTML = answerArray.join(" ");
-      letterCount--;
-      console.log("letterCount: " + letterCount);
-    }
+document.addEventListener("keyup", function(start) {
+  if (start.keyCode === 13 && gameStart === false && pause === true) {
+    gameStart = true;
+    initialize();
+    pause = false;
+    console.log("game has started initializing");
   }
-}
-  if (letterCount == 0) {
-    score++;
+});
+
+//global variables//
+var wordArray = ["cat", "dog", "sheep", "crow", "cow", "pig", "racoon"];
+var randomWord;
+var answerArray = [];
+var keyPress;
+var score = 0;
+var letterCount;
+var guessCanvas = [];
+var guessed;
+var lives = [];
+var match;
+var winState;
+
+function initialize() {
+  randomWord = wordArray[Math.floor(Math.random() * wordArray.length)];
+  letterCount = randomWord.length;
+  answerArray = [];
+  keyPress = [];
+  guessCanvas = [];
+  guessed = [];
+  lives = 5;
+  guessed = false;
+  console.log("randomWord: " + randomWord);
+  console.log("lettercount: " + letterCount);
+  for (var i = 0; i < letterCount; i++) {
+    answerArray[i] = "*";
+    document.getElementById("mask").innerHTML = answerArray.join(" ");
   }
   document.getElementById("score").innerHTML = score;
+  document.getElementById("lives").innerHTML = lives;
+}
 
+document.onkeyup = function(guessFun) {
+  if (guessFun.keyCode > 64 && guessFun.keyCode < 91) {
+    keyPress = guessFun.key;
+    console.log("keyPress: " + keyPress);
+    wordCheck();
+  } else {
+    console.log("error");
+  }
+};
+
+function wordCheck() {
+  guessed = guessCanvas.includes(keyPress);
+  console.log("guessCanvas: " + guessCanvas);
+  console.log("guessed: " + guessed);
+  compare();
+}
+
+function compare() {
+  if (guessed === false) {
+    guessCanvas = guessCanvas + keyPress;
+    matchCheck();
+  } else {
+    console.log("you already guessed that!");
+  }
+}
+
+function matchCheck() {
+  match = randomWord.includes(keyPress);
+  console.log("match: " + match);
+  replaceLetter();
+}
+
+function replaceLetter() {
+  if (match === true) {
+    for (var i = 0; i < randomWord.length; i++) {
+      if (randomWord[i] === keyPress) {
+        answerArray[i] = keyPress;
+        document.getElementById("mask").innerHTML = answerArray.join(" ");
+        console.log("randomWord index: " + randomWord[i]);
+        console.log("AnswerArray: " + answerArray);
+        maskCheck();
+      }
+    }
+  } else {
+    lives--;
+    document.getElementById("lives").innerHTML = lives;
+  }
+}
+
+function maskCheck() {
+  answerMaskOn = answerArray.includes("*");
+  winCheck();
+}
+
+function winCheck() {
+  if (answerMaskOn === false) {
+    score++;
+    document.getElementById("score").innerHTML = score;
+    console.log("Congratulation!");
+    pause();
+  }
 }
